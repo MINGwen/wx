b41a438051851627fffa3a482896739a888bfd3d
humingwen <L13141228@163.com>
2018-02-10 21:54:22 +0800

    change appkey

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 include_once "wxBizMsgCrypt.php";
 
 // 第三方发送消息给公众平台
-$encodingAesKey = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG";
-$token = "pamtest";
+$encodingAesKey = "tiStgietBexNAabo6IdqWBmm97ih1UU5H9IUwMlfRjP";
+$token = "humwen";
 $timeStamp = "1409304348";
 $nonce = "xxxxxx";
-$appId = "wxb11529c136998cb6";
+$appId = "c27bc385a7e6a5c2bd52012f243f0a4c";
 $text = "<xml><ToUserName><![CDATA[oia2Tj我是中文jewbmiOUlr6X-1crbLOvLw]]></ToUserName><FromUserName><![CDATA[gh_7f083739789a]]></FromUserName><CreateTime>1407743423</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[eYJ1MbwPRJtOvIEabaxHs7TX2D-HV71s79GUxqdUkjm6Gs2Ed1KF3ulAOA9H1xG0]]></MediaId><Title><![CDATA[testCallBackReplyVideo]]></Title><Description><![CDATA[testCallBackReplyVideo]]></Description></Video></xml>";
 
 
