5fb8340ff3550d6f4769f90eea1d05b0ef880f75
humingwen <humw@mama.cn>
2018-02-12 11:44:03 +0800

    have a resopone test

--- test.php
+++ test.php
@@ -1,161 +1,79 @@
 <?php
 
 /**
+ * wechat php test
+ */
 
-  * wechat php test
-
-  */
-
- 
 
 //define your token
 
 define("TOKEN", "humwen");
 
 $wechatObj = new wechatCallbackapiTest();
-
 $wechatObj->valid();
+$wechatObj->responseMsg();
 
- 
+class wechatCallbackapiTest {
 
-class wechatCallbackapiTest
-
-{
-
-public function valid()
-
-    {
-
+    public function valid() {
         $echoStr = $_GET["echostr"];
-
- 
-
         //valid signature , option
-
-        if($this->checkSignature()){
-
-        echo $echoStr;
-
-        exit;
-
+        if ($this->checkSignature()) {
+            echo $echoStr;
+            exit;
         }
-
     }
 
- 
 
-    public function responseMsg()
+    public function responseMsg() {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        //extract post data
+        if (!empty($postStr)) {
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag></xml>";
 
-    {
-
-//get post data, May be due to the different environments
-
-$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
- 
-
-      //extract post data
-
-if (!empty($postStr)){
-
-                
-
-              $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-
-                $fromUsername = $postObj->FromUserName;
-
-                $toUsername = $postObj->ToUserName;
-
-                $keyword = trim($postObj->Content);
-
-                $time = time();
-
-                $textTpl = "<xml>
-
-<ToUserName><![CDATA[%s]]></ToUserName>
-
-<FromUserName><![CDATA[%s]]></FromUserName>
-
-<CreateTime>%s</CreateTime>
-
-<MsgType><![CDATA[%s]]></MsgType>
-
-<Content><![CDATA[%s]]></Content>
-
-<FuncFlag>0</FuncFlag>
-
-</xml>";             
-
-if(!empty( $keyword ))
-
-                {
-
-              $msgType = "text";
-
+            if (!empty($keyword)) {
+                $msgType = "text";
                 $contentStr = "Welcome to wechat world!";
-
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-
                 echo $resultStr;
-
-                }else{
-
+            } else {
                 echo "Input something...";
-
-                }
-
- 
-
-        }else {
-
-        echo "";
-
-        exit;
-
+            }
+        } else {
+            echo "";
+            exit;
         }
-
     }
 
- 
-
-private function checkSignature()
-
-{
-
+    private function checkSignature()
+    {
         $signature = $_GET["signature"];
-
         $timestamp = $_GET["timestamp"];
-
         $nonce = $_GET["nonce"];
-
-       
-
-$token = TOKEN;
-
-$tmpArr = array($token, $timestamp, $nonce);
-
-sort($tmpArr);
-
-$tmpStr = implode( $tmpArr );
-
-$tmpStr = sha1( $tmpStr );
-
- 
-
-if( $tmpStr == $signature ){
-
-return true;
-
-}else{
-
-return false;
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
 
-}
-
-}
-
- 
 
 ?>
